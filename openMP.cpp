32d958ff55e51c1d43ef7f1306277838e26c70f8
BOB450 <bwvoskuil@gmail.com>
2021-09-30 15:36:09 -0600

    openMP now shows number of threads

--- openMP.cpp
+++ openMP.cpp
@@ -2,9 +2,12 @@
 #include <omp.h>
 int main()
 {
+    int i = 0;
     #pragma omp parallel
     {
         std::cout << "Hello World" << std::endl;
+        ++i;
     }
+    std::cout << "You have  " << i << " Logical Threads";
     return 0;
 }
